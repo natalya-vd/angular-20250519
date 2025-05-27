45e8a8a95a4154550c0d344ecf41ba1dc60882eb
Letto228 <letto1101@gmail.com>
2025-05-27 07:43:19 +0300

    chore: sanitize project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,9 @@ import {HeaderComponent} from './app-components/header/header.component';
 
 @Component({
     selector: 'app-root',
-    // selector: 'div#root',
     standalone: true,
     imports: [HeaderComponent],
     templateUrl: './app.component.html',
-    // template: '<h1>Its works</h1>',
     styleUrl: './app.component.css',
 })
 export class AppComponent {
