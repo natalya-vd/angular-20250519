7a61fd5152b10b7358b71e5226a8ff1980dcbc43
Letto228 <letto1101@gmail.com>
2025-05-27 07:46:08 +0300

    feat: add homework stand

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
 import {HeaderComponent} from './app-components/header/header.component';
+import {ProductsListComponent} from './pages/products-list/products-list.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [HeaderComponent],
+    imports: [HeaderComponent, ProductsListComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
 })
